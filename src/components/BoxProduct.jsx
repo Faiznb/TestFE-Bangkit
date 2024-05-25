710c8a393d0732dd068b1de7a17c9a77451a9324
unknown <faiznurbudi@gmail.com>
2024-05-25 19:01:00 +0700

    a

--- src/components/BoxProduct.jsx
+++ src/components/BoxProduct.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 const BoxProduct = ({ product }) => {
   return (
-    <Link to={`/detail/${product.id}`} className="w-40 border-black border-2 rounded-xl h-52 lg:w-52 lg:h-72 overflow-hidden">
+    <Link to={`/detail/${product.id}`} className="w-40 border-black border-2 rounded-xl h-52 lg:w-52 lg:h-72 overflow-hidden shadow-2xl">
       <img src={product.image} alt={product.name} className="w-40 h-40 lg:w-52 lg:h-52" />
       <div className="flex justify-between items-center mr-3 lg:mt-2 lg:text-lg text-sm">
         <p className=" ml-2">{product.name}</p>
